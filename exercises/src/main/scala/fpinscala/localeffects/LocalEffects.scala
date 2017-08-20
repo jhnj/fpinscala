0117d0a55101d5e7389b37f926d3e79f0b5d6f05
Johan Jern <johanjern95@gmail.com>
2017-08-20 14:53:48 +0300

    mutable state

--- exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -98,7 +98,18 @@ sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
   // Turn the array into an immutable list
   def freeze: ST[S,List[A]] = ST(value.toList)
 
-  def fill(xs: Map[Int,A]): ST[S,Unit] = ???
+  /** 14.1
+    * Add a combinator on STArray to fill the array from a Map where each key in the map
+    * represents an index into the array, and the value under that key is written to the array
+    * at that index. For example, xs.fill(Map(0->"a", 2->"b")) should write the value
+    * "a" at index 0 in the array xs and "b" at index 2. Use the existing combinators to write
+    * your implementation.
+    */
+  def fill(xs: Map[Int,A]): ST[S,Unit] = {
+    xs.foldLeft(ST[S,Unit](())) { case (st,(i, a)) =>
+      st.flatMap((_: Unit) => write(i, a))
+    }
+  }
 
   def swap(i: Int, j: Int): ST[S,Unit] = for {
     x <- read(i)
@@ -124,9 +135,36 @@ object STArray {
 object Immutable {
   def noop[S] = ST[S,Unit](())
 
-  def partition[S](a: STArray[S,Int], l: Int, r: Int, pivot: Int): ST[S,Int] = ???
+  /** 14.2
+    * Write the purely functional versions of partition and qs.
+    */
+  def partition[S](a: STArray[S, Int], l: Int, r: Int, pivot: Int): ST[S, Int] = for {
+    pivotVal <- a.read(pivot)
+    _ <- a.swap(pivot, r)
+    j <- STRef(l)
+    _ <- (l until r).foldLeft(noop[S]) { (st, i) =>
+      for {
+        _ <- st
+        arrI <- a.read(i)
+        _ <- if (arrI < pivotVal) for {
+          jValue <- j.read
+          _ <- a.swap(i, jValue)
+          _ <- j.write(jValue + 1)
+        } yield () else noop[S]
+      } yield ()
+    }
+    jValue <- j.read
+    _ <- a.swap(jValue, r)
+  } yield jValue
 
-  def qs[S](a: STArray[S,Int], l: Int, r: Int): ST[S, Unit] = ???
+
+  def qs[S](a: STArray[S,Int], l: Int, r: Int): ST[S, Unit] = for {
+    _ <- if (l < r) for {
+      pi <- partition(a, l, r, l + (r - l) / 2)
+      _ <- qs(a, l, pi - 1)
+      _ <- qs(a, pi + 1, r)
+    } yield () else noop[S]
+  } yield ()
 
   def quicksort(xs: List[Int]): List[Int] =
     if (xs.isEmpty) xs else ST.runST(new RunnableST[List[Int]] {
@@ -141,3 +179,41 @@ object Immutable {
 
 import scala.collection.mutable.HashMap
 
+/** 14.3
+  * Give the same treatment to scala.collection.mutable.HashMap as we’ve given here
+  * to references and arrays. Come up with a minimal set of primitive combinators for cre-
+  * ating and manipulating hash maps.
+  */
+sealed abstract class STHashMap[S,A,B] {
+  protected def value: scala.collection.mutable.HashMap[A,B]
+  def size: ST[S,Int] = ST(value.size)
+
+  // Write a value at the give index of the array
+  def update(a: A, b: B): ST[S,Unit] = new ST[S,Unit] {
+    def run(s: S) = {
+      value.update(a,b)
+      ((), s)
+    }
+  }
+
+  def remove(a: A): ST[S,Unit] = new ST[S,Unit] {
+    def run(s: S) = {
+      value.remove(a)
+      ((), s)
+    }
+  }
+
+  // Read the value at the given index of the array
+  def read(a: A): ST[S,B] = ST(value(a))
+
+  // Turn the array into an immutable list
+  def freeze: ST[S,scala.collection.immutable.Map[A,B]] = ST(value.toMap)
+}
+
+object STHashMap {
+  def fromKVPairs[S,A,B](xs: List[(A,B)]): ST[S, STHashMap[S,A,B]] =
+    ST(new STHashMap[S,A,B] {
+      val builder = HashMap.newBuilder[A,B] ++= xs
+      lazy val value = builder.result
+    })
+}
